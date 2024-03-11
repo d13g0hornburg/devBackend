a011ba7e55ebed573f991c2c4bb2e2fe0826ce04
Diego HB <69736334+d13g0hornburg@users.noreply.github.com>
2024-03-11 19:44:51 -0300

    Update exer14.js

--- exer/exer14.js
+++ exer/exer14.js
@@ -1,6 +1,6 @@
 let stringExer14 = "Desenvolvimento Backend!";
 console.log("Comprimento da expressão 'Desenvolvimento Backend!' em caracteres é: " + comprimentoString(stringExer14));
 
-function comprimentoString(string){
-    return string.length;
+function comprimentoString(stringExer14){
+    return stringExer14.length;
 }
